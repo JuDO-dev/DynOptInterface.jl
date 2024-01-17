47b79ef321e2e63e9abf005bfb5f0c754e629e82
Documenter.jl <documenter@juliadocs.github.io>
2024-01-17 11:30:05 +0000

    build based on 700a8b1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DynOptInterface","category":"page"},{"location":"#DynOptInterface","page":"Home","title":"DynOptInterface","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DynOptInterface.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DynOptInterface]","category":"page"}]
+}
